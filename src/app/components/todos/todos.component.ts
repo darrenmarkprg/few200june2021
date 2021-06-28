07804ed86282358a179b44062d0d196b94c9ae6a
Jeffry Gonzalez <jeff@hypertheory.com>
2021-06-28 14:24:22 -0400

    done with the basic list

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoListItemViewModel } from 'src/app/models/todos.models';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todoList: TodoListItemViewModel[] = [
+    { id: '1', description: 'Rake Leaves' },
+    { id: '2', description: 'Wash Deck' }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onItemAdded(thing: { item: string }) {
+    const newItem: TodoListItemViewModel = {
+      description: thing.item,
+      id: '99'
+    };
+
+    this.todoList = [newItem, ...this.todoList];
+
+  }
 }
